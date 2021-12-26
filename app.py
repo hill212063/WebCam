a0a8384067bf9695dd6f368500ff1cd2f6d760bd
hill212063 <hill_sup@hotmail.com>
2021-12-27 00:12:07 +0700

    finished main feature in admin site

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
-from flask import Flask,render_template,Response
+from flask import Flask,render_template,Response,request
+from flask_mysqldb import MySQL
 import cv2
 import time
-
-
+import uuid
+from datetime import datetime
 import imutils
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
-
-
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms import TimeField
-from wtforms.validators import DataRequired
+from wtforms import StringField,TextAreaField,DateField,TimeField,SubmitField,IntegerField
+from wtforms.validators import InputRequired,Length,NumberRange
+import os
 
-from flask_wtf.csrf import CSRFProtect
-#import os
-#SECRET_KEY = os.urandom(32)
-#app.config['SECRET_KEY'] = SECRET_KEY
 
 
 app=Flask(__name__)
 num_cams = [0,1]
-csrf = CSRFProtect(app)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
-class CreateUserForm(FlaskForm):
-    fname = StringField('fname', validators=[DataRequired()])
-    lname = StringField('lname', validators=[DataRequired()])
-    time = TimeField('receive time',validators=[DataRequired()])
+#MYSQL
+app.config['MYSQL_HOST'] = os.getenv('DATABASE_HOST')
+app.config['MYSQL_USER'] = os.getenv('DATABASE_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('DATABASE_NAME')
+mysql = MySQL(app)
+
+
 
 
 def gen(id):
@@ -41,23 +41,55 @@ def gen(id):
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 #ADMIN
-
-
-@app.route('/admin')
+@app.route('/admin/cameras')
 def admin():
-    return render_template('admin.html',num_cams=num_cams)
+    return render_template('admin_cameras.html',num_cams=num_cams)
 
-@app.route('/admin/<int:id>')
+@app.route('/admin/cameras/<int:id>')
 def video(id):
     return( Response(gen(id),mimetype='multipart/x-mixed-replace; boundary=frame'))
     
-@app.route('/', methods=['GET', 'POST'])
+
+class CreateUserForm(FlaskForm):
+    name = StringField('name', validators=[InputRequired(),Length(max=50)])
+    surname = StringField('surname', validators=[InputRequired(),Length(max=50)])
+    queue = DateField('queue',validators=[InputRequired()])
+    queue_time = TimeField('queue_time',validators=[InputRequired()])
+    pet_name = StringField('pet_name',validators=[InputRequired(),Length(max=20)])
+    pet_age = IntegerField('pet_age',validators=[InputRequired(),NumberRange(min=0,max=999)])
+    pet_type = StringField('pet_type',validators=[InputRequired(),Length(max=10)])
+    note = TextAreaField('note')
+    submit = SubmitField('submit')   
+
+@app.route('/', methods= ['GET','POST'])
 def submit():
-    form = MyForm()
+    form = CreateUserForm()
     if form.validate_on_submit():
-        return "<h1>Success</h1>"
+        name = form.name.data
+        surname = form.surname.data
+        queue_timestamp=str(form.queue.data)+" "+str(form.queue_time.data)
+        pet_name = form.pet_name.data
+        pet_age = form.pet_age.data
+        pet_type = form.pet_type.data
+        note = form.note.data
+        token = uuid.uuid4()
+        cursor = mysql.connection.cursor()
+        cursor.execute('''INSERT INTO `%s` ( `name`, `surname`, `queue_time`, `pet_name`, `pet_age`, `pet_type`, `note`, `token`)
+        VALUES ('%s','%s','%s','%s','%s','%s','%s','%s');'''
+        %(os.getenv('DATABASE_TABLE_NAME'),name,surname,queue_timestamp,pet_name,pet_age,pet_type,note,token))
+        mysql.connection.commit()
+        cursor.close()
+        return render_template('index.html', form=form)
     return render_template('index.html', form=form)
 
+@app.route('/admin/queue')
+def queue():
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT  name, surname, queue_time,status,pet_name,pet_age,pet_type,note FROM %s'''%(os.getenv('DATABASE_TABLE_NAME')))
+    mysql.connection.commit()
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('queue_manager.html',table=data)
+
 if __name__=="__main__":
-    app.run(threaded=True)
-    csrf.init_app(app)
+    app.run(debug=True)
