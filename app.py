35f1db75acedcc9e69c921c2709953856c999184
hill212063 <hill_sup@hotmail.com>
2021-12-28 16:15:30 +0700

    version0

--- app.py
+++ app.py
@@ -1,34 +1,57 @@
-from flask import Flask,render_template,Response,request
+from flask import Flask,render_template,Response,request,session,g,jsonify
 from flask_mysqldb import MySQL
+from flask.helpers import url_for
+#from flask_socketio import SocketIO,emit
+#from flask_cors import CORS
 import cv2
 import time
-import uuid
-from datetime import datetime
+from datetime import datetime,timedelta
 import imutils
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,DateField,TimeField,SubmitField,IntegerField
-from wtforms.validators import InputRequired,Length,NumberRange
+from wtforms.validators import InputRequired, Length,NumberRange
+from wtforms import StringField,TextAreaField,DateField,TimeField,SubmitField,IntegerField, PasswordField
+from werkzeug.utils import redirect
+
 import os
 
 
-
+now = datetime.now()
 app=Flask(__name__)
+#cors = CORS(app, resources={r"/*": {"origins": "*"}})
+#socketio= SocketIO(app,cors_allowed_origins='*')
 num_cams = [0,1]
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 
-#MYSQL
+########################   MYSQL   ##############################  
 app.config['MYSQL_HOST'] = os.getenv('DATABASE_HOST')
 app.config['MYSQL_USER'] = os.getenv('DATABASE_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('DATABASE_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('DATABASE_NAME')
 mysql = MySQL(app)
+########################   MYSQL   ##############################  
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[Length(max=20)])
+    password = PasswordField("Password",validators=[Length(max=40)])
+    submit = SubmitField('Login')
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(days=1)
+    g.user = None
+
+    if 'username' in session:
+        g.user = session['username']
 
 
-
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 def gen(id):
     vs = WebcamVideoStream(src=id).start()
@@ -40,7 +63,7 @@ def gen(id):
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-#ADMIN
+#######################################   ADMIN   #######################################  
 @app.route('/admin/cameras')
 def admin():
     return render_template('admin_cameras.html',num_cams=num_cams)
@@ -49,16 +72,17 @@ def admin():
 def video(id):
     return( Response(gen(id),mimetype='multipart/x-mixed-replace; boundary=frame'))
     
-
 class CreateUserForm(FlaskForm):
     name = StringField('name', validators=[InputRequired(),Length(max=50)])
     surname = StringField('surname', validators=[InputRequired(),Length(max=50)])
+    password = StringField('password',validators=[InputRequired(),Length(max=40)])
     queue = DateField('queue',validators=[InputRequired()])
     queue_time = TimeField('queue_time',validators=[InputRequired()])
     pet_name = StringField('pet_name',validators=[InputRequired(),Length(max=20)])
     pet_age = IntegerField('pet_age',validators=[InputRequired(),NumberRange(min=0,max=999)])
     pet_type = StringField('pet_type',validators=[InputRequired(),Length(max=10)])
     note = TextAreaField('note')
+    cam = IntegerField('cam',validators=[InputRequired(),NumberRange(min=0,max=1)])
     submit = SubmitField('submit')   
 
 @app.route('/', methods= ['GET','POST'])
@@ -67,29 +91,42 @@ def submit():
     if form.validate_on_submit():
         name = form.name.data
         surname = form.surname.data
+        pwd = form.password.data
         queue_timestamp=str(form.queue.data)+" "+str(form.queue_time.data)
         pet_name = form.pet_name.data
         pet_age = form.pet_age.data
         pet_type = form.pet_type.data
         note = form.note.data
-        token = uuid.uuid4()
+        cam = form.cam.data
         cursor = mysql.connection.cursor()
-        cursor.execute('''INSERT INTO `%s` ( `name`, `surname`, `queue_time`, `pet_name`, `pet_age`, `pet_type`, `note`, `token`)
-        VALUES ('%s','%s','%s','%s','%s','%s','%s','%s');'''
-        %(os.getenv('DATABASE_TABLE_NAME'),name,surname,queue_timestamp,pet_name,pet_age,pet_type,note,token))
+        cursor.execute('''INSERT INTO `%s` ( `name`, `surname`, `queue_time`, `pet_name`, `pet_age`, `pet_type`,`password`,`camera_id`, `note`)
+        VALUES ('%s','%s','%s','%s','%s','%s','%s',%s,'%s');'''
+        %(os.getenv('DATABASE_TABLE_CLIENT_NAME'),name,surname,queue_timestamp,pet_name,pet_age,pet_type, pwd,cam ,note))
         mysql.connection.commit()
         cursor.close()
         return render_template('index.html', form=form)
     return render_template('index.html', form=form)
 
-@app.route('/admin/queue')
-def queue():
+
+@app.route('/admin/queue_management')
+def queue_management():
     cursor = mysql.connection.cursor()
-    cursor.execute('''SELECT  name, surname, queue_time,status,pet_name,pet_age,pet_type,note FROM %s'''%(os.getenv('DATABASE_TABLE_NAME')))
+    cursor.execute('''SELECT  name, surname, queue_time,status,pet_name,pet_age,pet_type,camera_id,note FROM %s'''%(os.getenv('DATABASE_TABLE_CLIENT_NAME')))
     mysql.connection.commit()
     data = cursor.fetchall()
     cursor.close()
-    return render_template('queue_manager.html',table=data)
+    return render_template('queue_management.html',table=data)
+#######################################   ADMIN   #######################################  
+
+
+#######################################   user   ########################################
+@app.route('/user/camera/<int:id>')
+def user_video(id):
+    return( Response(gen(id),mimetype='multipart/x-mixed-replace; boundary=frame'))
+#######################################   user   ########################################
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
+
