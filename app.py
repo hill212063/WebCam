a49b597dc068ff2b71200c858fbeb5c1eaccb72e
hill212063 <hill_sup@hotmail.com>
2021-12-31 00:32:25 +0700

    version0.4

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ from datetime import datetime,timedelta
 import imutils
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
-from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired, Length,NumberRange
-from wtforms import StringField,TextAreaField,DateField,TimeField,SubmitField,IntegerField, PasswordField
+#from flask_wtf import FlaskForm
+#from wtforms.validators import InputRequired, Length,NumberRange
+#from wtforms import StringField,TextAreaField,DateField,TimeField,SubmitField,IntegerField, PasswordField
+from flask_qrcode import QRcode
 import os
 
 
@@ -32,25 +33,6 @@ app.config['MYSQL_DB'] = os.getenv('DATABASE_NAME')
 mysql = MySQL(app)
 ########################   MYSQL   ##############################  
 
-class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[Length(max=20)])
-    password = PasswordField("Password",validators=[Length(max=40)])
-    submit = SubmitField('Login')
-
-@app.before_request
-def before_request():
-    session.permanent = True
-    app.permanent_session_lifetime = timedelta(days=1)
-    g.user = None
-
-    if 'username' in session:
-        g.user = session['username']
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
 def gen(id):
     vs = WebcamVideoStream(src=id).start()
     while True:
@@ -62,6 +44,30 @@ def gen(id):
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 #######################################   ADMIN   #######################################  
+@app.route('/admin/chk_cam', methods= ['GET'])
+def check_cam_busy():
+    cam={}
+    for i in num_cams:
+        cam[str(i)]=False
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT camera_id FROM `%s` WHERE `status`='Wait' '''%(os.getenv('DATABASE_TABLE_CLIENT_NAME')))
+    mysql.connection.commit()
+    data = cursor.fetchall()
+    cursor.close()
+    for i in data:
+        key = str(i[0])
+        cam[key]=True
+    return jsonify(cam)
+
+def check_cam_busy_invidual(cam_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT camera_id FROM `%s` WHERE `status`='Wait' AND `camera_id`='%s' '''%(os.getenv('DATABASE_TABLE_CLIENT_NAME'),cam_id))
+    mysql.connection.commit()
+    data = cursor.fetchall()
+    cursor.close()
+    if(data): return True
+    else: return False
+
 @app.route('/admin/cameras')
 def admin_cameras():
     return render_template('admin_cameras.html',num_cams=num_cams)
@@ -69,42 +75,29 @@ def admin_cameras():
 @app.route('/admin/cameras/<int:id>')
 def video(id):
     return( Response(gen(id),mimetype='multipart/x-mixed-replace; boundary=frame'))
-    
-class UserForm(FlaskForm):
-    name = StringField('name', validators=[InputRequired(),Length(max=50)])
-    surname = StringField('surname', validators=[InputRequired(),Length(max=50)])
-    password = StringField('password',validators=[InputRequired(),Length(max=40)])
-    queue = DateField('queue',validators=[InputRequired()])
-    queue_time = TimeField('queue_time',validators=[InputRequired()])
-    pet_name = StringField('pet_name',validators=[InputRequired(),Length(max=20)])
-    pet_age_year = IntegerField('pet_age_year',validators=[InputRequired(),NumberRange(min=0,max=99)])
-    pet_age_month = IntegerField('pet_age_month',validators=[InputRequired(),NumberRange(min=0,max=11)])
-    pet_type = StringField('pet_type',validators=[InputRequired(),Length(max=10)])
-    note = TextAreaField('note')
-    cam = IntegerField('cam',validators=[InputRequired(),NumberRange(min=0,max=1)])
-    submit = SubmitField('submit')   
-
 
 @app.route('/', methods= ['GET','POST'])
 def submit():
-    form = UserForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        surname = form.surname.data
-        pwd = form.password.data
-        queue_timestamp=str(form.queue.data)+" "+str(form.queue_time.data)
-        pet_name = form.pet_name.data
-        pet_age = str(form.pet_age_year.data)+":"+str(form.pet_age_month.data)
-        pet_type = form.pet_type.data
-        note = form.note.data
-        cam = form.cam.data
+    if request.method == 'POST':
+        name = request.form['name']
+        surname = request.form['surname']
+        pwd = request.form['password']
+        queue_timestamp=request.form['queue']
+        pet_name = request.form['pet_name']
+        pet_age = str(request.form['pet_age_year'])+':'+str(request.form['pet_age_month']) 
+        pet_type = request.form['pet_type']
+        cam = request.form['cam_id']
+        if(check_cam_busy_invidual(cam)):
+            flash("This camera is Busy!!")
+            return render_template('admin_dashboard.html',num_cams=num_cams)
+        note = request.form['note']
         cursor = mysql.connection.cursor()
         cursor.execute('''INSERT INTO `%s` ( `name`, `surname`, `queue_time`, `pet_name`, `pet_age`, `pet_type`,`password`,`camera_id`, `note`)
         VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s');'''
         %(os.getenv('DATABASE_TABLE_CLIENT_NAME'),name,surname,queue_timestamp,pet_name,pet_age,pet_type, pwd,cam ,note))
         mysql.connection.commit()
         cursor.close()
-    return render_template('index.html', form=form)
+    return render_template('admin_dashboard.html',num_cams=num_cams)
 
 
 @app.route('/admin/queue', methods= ['GET','POST'])
@@ -114,7 +107,7 @@ def queue():
     mysql.connection.commit()
     data = cursor.fetchall()
     cursor.close()
-    return render_template('admin_queue.html',table=data)
+    return render_template('admin_queue.html',table=data,num_cams=num_cams)
 
 
 @app.route('/admin/queue/update',methods = ['GET', 'POST'])
@@ -122,25 +115,25 @@ def update_table():
     if request.method == 'POST':
         row_id=request.form['id']
         name = request.form['name']
-        surname=request.form['id']
+        surname=request.form['surname']
         queue_timestamp = request.form['queue']
         pet_name = request.form['pet_name']
-        pet_age = request.form['pet_age']
+        pet_age = str(request.form['pet_age_year'])+':'+str(request.form['pet_age_month']) 
         pet_type = request.form['pet_type']
-        cam = request.form['cam_id']
         note = request.form['note']
         cursor = mysql.connection.cursor()
         cursor.execute('''UPDATE `%s` SET `name`='%s',`surname`='%s',`queue_time`='%s',
-        `pet_name`='%s',`pet_age`='%s',`pet_type`='%s',`camera_id`='%s',`note`='%s' WHERE `id`='%s' '''
-        %(os.getenv('DATABASE_TABLE_CLIENT_NAME'),name,surname,queue_timestamp,pet_name,pet_age,pet_type,cam ,note,row_id))
+        `pet_name`='%s',`pet_age`='%s',`pet_type`='%s',`note`='%s' WHERE `id`='%s' '''
+        %(os.getenv('DATABASE_TABLE_CLIENT_NAME'),name,surname,queue_timestamp,pet_name,pet_age,pet_type ,note,row_id))
         mysql.connection.commit()
         cursor.close()
     return redirect(url_for('queue'))
 
-@app.route('/admin/queue/delete/<int:id>',methods = ['GET', 'POST'])
-def delete_table(row_id):
+@app.route('/admin/queue/delete/<id>/',methods = ['GET', 'POST'])
+def delete_table(id):
+    print("Hello")
     cursor = mysql.connection.cursor()
-    cursor.execute('''DELETE FROM `%s` WHERE `id`='%s' '''%(os.getenv('DATABASE_TABLE_CLIENT_NAME'),row_id))
+    cursor.execute('''DELETE FROM `%s` WHERE `id`='%s' '''%(os.getenv('DATABASE_TABLE_CLIENT_NAME'),id))
     mysql.connection.commit()
     cursor.close()
     return redirect(url_for('queue'))
